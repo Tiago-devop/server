416e2f3a59138891e330c0213513725d1d0e280e
Tiago-devop <tiagodev@protonmail.com>
2021-03-04 15:46:29 -0300

    chore: creating a MongoDB database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const server = require("./authpassword");
 
 // setup express server
 
@@ -12,3 +13,17 @@ app.listen(5000, () => console.log("Server started on port 5000"));
 // set up routers
 
 app.use("/snippet", require("./routes/snippetRouter"));
+
+// connect to mongoDB
+
+mongoose.connect(
+  `mongodb+srv://${server.nameServer}:${server.password}@snippet-manager.2uczn.mongodb.net/main?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) return console.error(err);
+    console.log("Connected to MongoDB");
+  }
+);
