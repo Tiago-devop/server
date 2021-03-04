c542eb1db6df00bbfdf260218c54ff0151bbe7e5
Tiago-devop <tiagodev@protonmail.com>
2021-03-04 14:32:14 -0300

    chore: creating the snippet database model

--- routes/snippetRouter.js
+++ routes/snippetRouter.js
@@ -1,8 +1,16 @@
 const router = require("express").Router();
+const Snippet = require("../models/snippetModel");
 
 router.post("/", (req, res) => {
   const { title, description, code } = req.body;
-  
+
+  const newSnippet = new Snippet({
+    title,
+    description,
+    code,
+  });
+
+  newSnippet.save();
 });
 
 module.exports = router;
