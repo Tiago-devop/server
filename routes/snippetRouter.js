cacc0f25fa248590dfb3f18f909cfa2dc722af24
Tiago-devop <tiagodev@protonmail.com>
2021-03-04 17:32:29 -0300

    chore: validating new snippets

--- routes/snippetRouter.js
+++ routes/snippetRouter.js
@@ -1,16 +1,30 @@
 const router = require("express").Router();
 const Snippet = require("../models/snippetModel");
 
-router.post("/", (req, res) => {
-  const { title, description, code } = req.body;
+router.post("/", async (req, res) => {
+  try {
+    const { title, description, code } = req.body;
 
-  const newSnippet = new Snippet({
-    title,
-    description,
-    code,
-  });
+    // validation
 
-  newSnippet.save();
+    if (!description && !code) {
+      return res.status(400).json({
+        errorMessage: "You need to enter at least a description or some code.",
+      });
+    }
+
+    const newSnippet = new Snippet({
+      title,
+      description,
+      code,
+    });
+
+    const savedSnippet = await newSnippet.save();
+
+    res.json(savedSnippet);
+  } catch (err) {
+    res.status(500).send();
+  }
 });
 
 module.exports = router;
