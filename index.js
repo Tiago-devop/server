df897acd881c3aa89844705be360e8216cff0ef6
Tiago-devop <tiagodev@protonmail.com>
2021-03-09 16:42:01 -0300

    fix: cors

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.listen(5000, () => console.log("Server started on port 5000"));
 
